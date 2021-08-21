853377afa6756df2514f2c160ecb6c77522950cc
someblonde <77970860+someblonde@users.noreply.github.com>
2021-08-21 15:40:58 +0100

    added working search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-
+import Dictionary from "./Dictionary"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-       Hello
-    
+  
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Laura E</small>
+      </footer>
+    </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary(){
+    let [keyword, setKeyword] = useState("");
+
+
+    function handleKeywordChange(event){
+        setKeyword(event.target.value);
+    }
+
+
+    function search (event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`)
+    }
+
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange} />
+            </form>
+         </div>
+      
+    )    
+}
