55f9d2111adc1fcb650c073b53b09a125d9b63df
someblonde <77970860+someblonde@users.noreply.github.com>
2021-08-23 12:51:34 +0100

    added axios and api and centered form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
 
 
-    function handleKeywordChange(event){
-        setKeyword(event.target.value);
-    }
+   function handleResponse(response) {
+       console.log(response.data[0]);
+   }
+    
 
 
     function search (event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`)
+       
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
     }
+
+     function handleKeywordChange(event){
+        setKeyword(event.target.value);
+     }
 
 
     return (
