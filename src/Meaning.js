364bff16dc1d5fddb503999ebacf1d093d9ddc7e
someblonde <77970860+someblonde@users.noreply.github.com>
2021-08-24 19:52:14 +0100

    created synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning (props){
     console.log(props.meaning);
@@ -11,8 +12,9 @@ export default function Meaning (props){
               <p class="lead">
               {definition.definition}
               <br />
-            
+              <strong>Example</strong>
               <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
               </p>
               </div>
          );
